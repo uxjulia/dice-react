b4e27707cf4db3713138609d7a5141384fe4a877
Julia Nguyen <julia@8thsq.co>
2016-12-26 14:43:31 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
-import Graph from './BarGraph.js';
-import Header from './Header.js';
-import PlayerIndicator from './PlayerIndicator.js';
-import DiceInput from './DiceInput.js';
-import Settings from './Settings.js';
+import Graph from './components/BarGraph.js';
+import Header from './components/Header.js';
+import PlayerIcons from './components/PlayerIcons.js';
+import DiceInput from './components/DiceInput.js';
+import Settings from './components/Settings.js';
+import Footer from './components/Footer.js';
+
+function Player(id, name){
+  this.id = id;
+  this.name = name;
+}
 
 function Layout(props) {
   return(
@@ -17,6 +23,7 @@ function Layout(props) {
           <div className="col-md-4">
             {props.right}
           </div>
+          {props.footer}
         </div>
       </div>
     </div>
@@ -33,65 +40,72 @@ class App extends Component {
     };
   }
 
-  updateLog = (n) => {
-    const data = this.state.rolls;
-    var x = n + ", ";
-    data.push(x);
-    return data;
-  }
-
   setPlayerNames = (e) => {
     const data = this.state.players;
     const x = Number(e.target.id) - 1;
     data[x] = {key: x, name: e.target.value};
     this.setState({players: data});
-  }
+  };
 
   setNextPlayerName = (e) => {
     this.setState({nextUp: e.target.title});
-  }
+  };
 
-  handleClick = () => {
+  handleClick = (e) => {
     if (this.state.players.length > 0) {
       this.setNext();
     }
-  }
+  };
+
+  handleDefaultPlayers = (e) => {
+    const players = this.state.players;
+    const val = e.target.value;
+    let x = 0;
+    while (x < val) {
+        const n = x + 1;
+        players[x] = {key: x, name: "Player " + n};
+        this.setState({players: players[x]});
+        x++;
+    }
+    const firstPlayer = this.state.players[0].name;
+    this.setState({nextUp: firstPlayer});
+};
 
   handleUndo = () => {
     const data = this.state.rolls;
     data.pop();
     this.setState({rolls: data});
-  }
+  };
 
   handleReset = () => {
     this.setState({rolls: []});
-  }
+  };
 
   getPlayer = () => {
     const players = this.state.players;
     const currentPlayer = this.state.nextUp;
-    var nx = players.find(function(players) {
+    return players.find(function(players) {
         return players.name === currentPlayer;
     });
-    return nx; 
-  }
+  };
 
   setNext = () => {
     const players = this.state.players;
     const thisPlayer = this.getPlayer();
     var x = (thisPlayer.key) + 1;
-    if (players.length === 1 || x == players.length || x === undefined) x = 0;
-    var nextPerson = players.find(function(players){   
+    if (players.length === 1 || x === players.length || x === undefined) x = 0;
+    let nextPerson = players.find(function(players){
       return players.key === x;
     });
     const nextName = nextPerson.name;
     this.setState({nextUp: nextName})
-  }
+  };
 
-  saveSettings = () => {
+  saveSettings = (e) => {
     const firstPlayer = this.state.players[0].name;
     this.setState({nextUp: firstPlayer});
-  }
+    e.preventDefault();
+  };
 
   render() {
     const data = this.state.rolls;
@@ -102,15 +116,13 @@ class App extends Component {
         <div>
           <div>
             <div className="form-group">
-              <PlayerIndicator onClick={this.setNextPlayerName} players={players} nextUp={nextUp}/>
+            <PlayerIcons onClick={this.setNextPlayerName} players={players} nextUp={nextUp}/>
             </div>
             <DiceInput onClick={this.handleClick}/>
-            <div className="card card-block">
-              <Settings onChange={this.setPlayerNames} onClick={this.saveSettings} />
-            </div>
+            <Settings onChange={this.setPlayerNames} onClick={this.saveSettings} />
             </div>
         </div>
-      }/>
+      } footer={<Footer/>}/>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import '../node_modules/tether/dist/js/tether.js';
-import '../node_modules/tether/dist/js/tether.js';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import '../node_modules/font-awesome-4.7.0/css/font-awesome.css';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  );
