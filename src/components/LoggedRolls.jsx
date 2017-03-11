6789910b95029e4d777172ea5e1906ffac13b9d2
Julia Nguyen <julia@8thsq.co>
2017-03-11 17:27:24 -0500

    Added total number of rolls logged

--- src/components/LoggedRolls.jsx
+++ src/components/LoggedRolls.jsx
@@ -1,17 +1,27 @@
 import React, {Component} from 'react';
-var _ = require('lodash');
+let _ = require('lodash');
 
 class LoggedRolls extends Component {
     render(){
     const data = this.props.data;
     const tail = _.tail(data);
     const first = _.head(data);
-      return(
-          <div className="card card-block" id="loggedRolls">
-                <span>Last Roll: </span>{!data.length && <span className="text-muted">No rolls yet</span>}
-                {data.length <= 1 && <span>{_.join(data, ', ')}</span>}
-                {data.length > 1 && <span className="rolllog"><span>{first}, </span><span>{_.join(tail, ', ')}</span></span>}
-          </div>
+    const style = {
+        total: {
+            fontSize: ".8rem"
+        }
+    };
+        return(
+            <div className="card card-block" id="loggedRolls">
+                <div className="form-group">
+                    <span className="text-muted" style={style.total}>Total Rolls: {data.length} </span>
+                </div>
+                <div>
+                    <span>Last Roll: </span>{!data.length && <span className="text-muted">No rolls yet</span>}
+                    {data.length <= 1 && <span>{_.join(data, ', ')}</span>}
+                    {data.length > 1 && <span className="rolllog"><span>{first}, </span><span>{_.join(tail, ', ')}</span></span>}
+              </div>
+        </div>
       )
   }
 }
